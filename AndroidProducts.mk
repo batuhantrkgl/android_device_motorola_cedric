0080e8dc514da6c5bb3804d5ff52aa35449a46f9
SirRGB <sirrgb@amyrom.ml>
2022-02-15 16:51:36 +0100

    cedric: change sources to arrow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cedric.mk
+    $(LOCAL_DIR)/arrow_cedric.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_cedric-user \
-    lineage_cedric-userdebug \
-    lineage_cedric-eng
+    arrow_cedric-user \
+    arrow_cedric-userdebug \
+    arrow_cedric-eng
